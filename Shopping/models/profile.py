6cac5bf936f533e1a113aced6256aebd961989eb
Prabesh Thapa <numb1prabesht7@gmail.com>
2022-04-17 19:58:08 +0545

    Profile Model Updated

--- Shopping/models/profile.py
+++ Shopping/models/profile.py
@@ -7,6 +7,7 @@ class Profile(models.Model):
     images = models.ImageField(default='static/UserProfile/user.png', upload_to='static/UserProfile')
     address = models.CharField(max_length=150, null=True, blank=True)
     age = models.IntegerField(null=True, blank=True)
+    created_at = models.DateField(auto_now_add=True, blank=True, null=True)
 
     def __str__(self):
         return self.customer.first_name
