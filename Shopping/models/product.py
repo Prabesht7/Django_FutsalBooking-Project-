e078d33573a5b2b8c22ee363ec44100a652675b5
Prabesh Thapa <numb1prabesht7@gmail.com>
2022-04-17 19:57:51 +0545

    Product Model Updated

--- Shopping/models/product.py
+++ Shopping/models/product.py
@@ -9,6 +9,9 @@ class Product(models.Model):
     description = models.CharField(max_length=300, default='', null=True, blank=True)
     image = models.ImageField(upload_to='uploads/products/')
 
+    def __str__(self):
+        return self.name
+
     @staticmethod
     def get_product_by_id(ids):
         return Product.objects.filter(id__in=ids)
