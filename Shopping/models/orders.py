def80cd34da96edece371b6dc5dcfd01ad0e6b56
Prabesh Thapa <numb1prabesht7@gmail.com>
2022-04-17 19:57:31 +0545

    Order Model Updated

--- Shopping/models/orders.py
+++ Shopping/models/orders.py
@@ -15,7 +15,7 @@ class Order(models.Model):
     phone = models.CharField(max_length=50, default='', blank=True)
     date = models.DateField(default=datetime.datetime.today)
     status = models.BooleanField(default=False)
-
+    ordered_at = models.DateField(auto_now_add=True, blank=True, null=True)
     def placeOrder(self):
         self.save()
 
