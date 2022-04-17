bedda39d6cdbf01e8032dc90a162fb76f1ef68f8
Prabesh Thapa <numb1prabesht7@gmail.com>
2022-04-17 19:53:44 +0545

    Customer Review Page Update (Players can send review which will display in About us Page)

--- Infofutsal/models/review.py
+++ Infofutsal/models/review.py
@@ -4,11 +4,12 @@ from django.db import models
 class Review(models.Model):
     fullname = models.CharField(max_length=150)
     message = models.CharField(max_length=350)
-    photo = models.ImageField(upload_to='static/images/reviewphoto')
+    photo = models.ImageField(upload_to='static/images/reviewphoto', default='static/images/reviewphoto/review.jpg',
+                              blank=True,
+                              null=True)
 
     def __str__(self):
         return self.fullname
-
 
     @staticmethod
     def get_all_reviews():
--- Infofutsal/views/review.py
+++ Infofutsal/views/review.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from Infofutsal.models.review import Review
 from django.contrib import messages
 from django.views import View
@@ -7,17 +7,19 @@ from Shopping.models.customer import Customer
 
 class review(View):
     def get(self, request):
-        customer = Customer.get_all_customers();
-        return render(request, 'review.html', {'customer': customer})
+        customer = Customer.get_all_customers()
+        review_list = Review.get_all_reviews()
+        return render(request, 'review.html', {'customer': customer, 'review_list': review_list})
 
     def post(self, request):
         review = Review()
         fullname = request.POST.get('fullname')
         message = request.POST.get('message')
-        photo = request.POST.get('photo')
+        if len(request.FILES) != 0:
+            review.photo = request.FILES['photo']
+        #photo = request.POST.get('photo')
         review.fullname = fullname
         review.message = message
-        review.photo = photo
         review.save()
         messages.success(request, 'Your Review have been successfully added on About Us page!')
-        return render(request, 'review.html')
+        return redirect('aboutus')
